338b6e08546c5f05dc09daee1205ab3c0febe66e
kaifkh20 <kaifkhan.biz@gmail.com>
2024-06-15 19:30:42 +0530

    Sphere ray tracing

--- ray.h
+++ ray.h
@@ -3,21 +3,22 @@
 
 #include "vec3.h"
 
-class ray{
-    private:
-        point orig;
-        vec3 dir;
-    public:
-        ray(){}
-        ray(const point &origin,const vec3 &dir): orig(origin),dir(dir){}
-        
-        const point& origin() const{ return orig;}
-        const vec3& direction() const{return dir;}
+class ray {
+  public:
+    ray() {}
 
-        point at(double t) const{
-            return orig + t*dir;
-        }
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
+    const point3& origin() const  { return orig; }
+    const vec3& direction() const { return dir; }
+
+    point3 at(double t) const {
+        return orig + t*dir;
+    }
+
+  private:
+    point3 orig;
+    vec3 dir;
 };
 
 #endif
