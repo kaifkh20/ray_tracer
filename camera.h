2591c414b79228f5059b887b3a49fa9267197950
kaifkh20 <kaifkhan.biz@gmail.com>
2024-06-19 17:02:50 +0530

    error fix

--- camera.h
+++ camera.h
@@ -4,13 +4,15 @@
 #include "rt_const.h"
 #include "hittable.h"
 
-class camera{
-    public:
+class camera {
+  public:
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int    image_width  = 100;  // Rendered image width in pixel count
-    void render(const hittable& world){
+
+    void render(const hittable& world) {
         initialize();
-        std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+        std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
@@ -19,7 +21,7 @@ class camera{
                 auto ray_direction = pixel_center - center;
                 ray r(center, ray_direction);
 
-                color pixel_color = ray_color(r,world);
+                color pixel_color = ray_color(r, world);
                 write_color(std::cout, pixel_color);
             }
         }
@@ -27,41 +29,38 @@ class camera{
         std::clog << "\rDone.                 \n";
     }
 
-    private:
-        int    image_height;   // Rendered image height
-        point3 center;         // Camera center
-        point3 pixel00_loc;    // Location of pixel 0, 0
-        vec3   pixel_delta_u;  // Offset to pixel to the right
-        vec3   pixel_delta_v;  // Offset to pixel below
-    
-        void initialize(){
-            auto aspect_ratio = 16.0 / 9.0;
-            int image_width = 400;
+  private:
+    int    image_height;   // Rendered image height
+    point3 center;         // Camera center
+    point3 pixel00_loc;    // Location of pixel 0, 0
+    vec3   pixel_delta_u;  // Offset to pixel to the right
+    vec3   pixel_delta_v;  // Offset to pixel below
 
-            // Calculate the image height, and ensure that it's at least 1.
-            int image_height = int(image_width / aspect_ratio);
-            image_height = (image_height < 1) ? 1 : image_height;
+    void initialize() {
+        image_height = int(image_width / aspect_ratio);
+        image_height = (image_height < 1) ? 1 : image_height;
 
-        // Camera
+        center = point3(0, 0, 0);
 
-            auto focal_length = 1.0;
-            auto viewport_height = 2.0;
-            auto viewport_width = viewport_height * (double(image_width)/image_height);
-            auto camera_center = point3(0, 0, 0);
+        // Determine viewport dimensions.
+        auto focal_length = 1.0;
+        auto viewport_height = 2.0;
+        auto viewport_width = viewport_height * (double(image_width)/image_height);
 
-            // Calculate the vectors across the horizontal and down the vertical viewport edges.
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        auto viewport_u = vec3(viewport_width, 0, 0);
+        auto viewport_v = vec3(0, -viewport_height, 0);
 
-            // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-            auto pixel_delta_u = viewport_u / image_width;
-            auto pixel_delta_v = viewport_v / image_height;
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        pixel_delta_u = viewport_u / image_width;
+        pixel_delta_v = viewport_v / image_height;
 
-            // Calculate the location of the upper left pixel.
-            auto viewport_upper_left = camera_center
-                                    - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-            auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+        // Calculate the location of the upper left pixel.
+        auto viewport_upper_left =
+            center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+        pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
+
 
     color ray_color(const ray& r,const hittable& world) const{
         hit_record rec;
