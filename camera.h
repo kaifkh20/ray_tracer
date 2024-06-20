9f45ff77c69a871746b69dd2dc84121719ccc184
Kaif Khan <127656823+kaifkh20@users.noreply.github.com>
2024-06-20 20:23:44 +0530

    Add files via upload

--- camera.h
+++ camera.h
@@ -9,6 +9,7 @@ class camera {
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int    image_width  = 100;  // Rendered image width in pixel count
     int samples_per_pixel = 10;
+    int max_depth = 10;
     void render(const hittable& world) {
         initialize();
 
@@ -20,7 +21,7 @@ class camera {
                 color pixel_color(0,0,0);
                 for(int sample=0;sample<samples_per_pixel;++sample){
                   ray r = get_ray(i,j);
-                  pixel_color+=ray_color(r,world);
+                  pixel_color+=ray_color(r,max_depth,world);
                 }
                 // 
                 // auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
@@ -83,10 +84,15 @@ class camera {
       return vec3(random_double()-0.5,random_double()-0.5,0);
     }
 
-    color ray_color(const ray& r,const hittable& world) const{
+    color ray_color(const ray& r,int depth,const hittable& world) const{
+        if(depth<=0){
+          return color(0,0,0);
+        }
+
         hit_record rec;
-        if (world.hit(r, interval(0,infinity), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
+        if (world.hit(r, interval(0.001,infinity), rec)) {
+          vec3 direction = random_on_hemisphere(rec.normal);
+          return 0.1 * ray_color(ray(rec.p,direction),depth-1,world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
